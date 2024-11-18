cff91bb3a8eb088a88cd9c636bc6ccd701f9acb7
kajanihon <kajanihon@gmail.com>
2024-11-18 16:49:48 -0300

    Initial commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MTMDBApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
